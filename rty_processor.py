f03633d69511fbc945d30773062cecc93d3436ce
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-02-19 09:25:53 +0700

    Update rty_processor.py

--- rty_processor.py
+++ rty_processor.py
@@ -175,6 +175,7 @@ def process_rty_7z(uploaded_file):
 
 
 
+        print("RETURNING 4 VALUES")
 
         if not all_data:
             return None, None, None, None
@@ -215,5 +216,6 @@ def process_rty_7z(uploaded_file):
     finally:
         if os.path.exists(temp_dir):
             shutil.rmtree(temp_dir)
+
 
 
